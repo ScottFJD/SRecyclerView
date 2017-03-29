950c4198d4c8984c9174ca1425194d0ee6e0ac40
fujindong <alanfujindong@gmail.com>
2017-03-29 16:30:27 +0800

    Initial commit

---
+++ app/src/main/java/com/scottfu/srecyclerview/adapter/BaseRecyclerAdapter.java
@@ -0,0 +1,24 @@
+package com.scottfu.srecyclerview.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by fujindong on 2017/3/29.
+ */
+
+public class BaseRecyclerAdapter extends RecyclerView.Adapter<> {
+
+}
+
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+
+    TextView mTextView;
+
+    public MyViewHolder(View itemView) {
+        super(itemView);
+        mTextView = itemView.findViewById()
+    }
+}
