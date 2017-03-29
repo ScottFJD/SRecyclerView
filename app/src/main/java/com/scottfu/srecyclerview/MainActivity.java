ca4ff88d90f5a029b13c3d8b912d2ba320905bcf
fujindong <alanfujindong@gmail.com>
2017-03-29 22:49:27 +0800

    listRecycleView

--- app/src/main/java/com/scottfu/srecyclerview/MainActivity.java
+++ app/src/main/java/com/scottfu/srecyclerview/MainActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.scottfu.srecyclerview.adapter.BaseRecyclerAdapter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private List<String> mDatas;
     private BaseRecyclerAdapter mRecyclerAdapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,43 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        initDatas();
+        initView();
+
+        mRecyclerAdapter = new BaseRecyclerAdapter(this, mDatas);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        mRecyclerAdapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Click"+position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "LongClick"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void initView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_content);
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i <= 'z'; i++) {
+            mDatas.add(""+(char)i);
+        }
     }
 
     @Override
@@ -50,9 +96,18 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (id) {
+            case R.id.action_add:
+                mRecyclerAdapter.addData(1);
+                return true;
+            case R.id.action_delete:
+                mRecyclerAdapter.deleteItem(1);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/scottfu/srecyclerview/adapter/BaseRecyclerAdapter.java
+++ app/src/main/java/com/scottfu/srecyclerview/adapter/BaseRecyclerAdapter.java
@@ -1,15 +1,95 @@
 package com.scottfu.srecyclerview.adapter;
 
+import android.content.Context;
+import android.content.pm.LabeledIntent;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.scottfu.srecyclerview.R;
+
+import java.util.List;
 
 /**
  * Created by fujindong on 2017/3/29.
  */
 
-public class BaseRecyclerAdapter extends RecyclerView.Adapter<> {
+public class BaseRecyclerAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
+    private List<String> mDatas;
+    private LayoutInflater mIntlater;
+    private Context mContext;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+
+        void onItemLongClick(View view, int position);
+    }
+
+    private OnItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    public BaseRecyclerAdapter(Context context, List<String> datas) {
+        mContext = context;
+        mDatas = datas;
+        mIntlater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mIntlater.inflate(R.layout.item_single_textview, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.mTextView.setText(mDatas.get(position));
+
+        setUpItemEvent(holder);
+    }
+
+    protected void setUpItemEvent(final MyViewHolder holder) {
+        if (onItemClickListener != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    onItemClickListener.onItemClick(holder.itemView,pos);
+                }
+            });
+
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    onItemClickListener.onItemLongClick(holder.itemView, pos);
+                    return false;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public void addData(int position) {
+        mDatas.add(position,"Insert one");
+        notifyItemInserted(position);
+    }
+
+    public void deleteItem(int position) {
+        mDatas.remove(position);
+        notifyItemRemoved(position);
+    }
 }
 
 
@@ -19,6 +99,6 @@ class MyViewHolder extends RecyclerView.ViewHolder {
 
     public MyViewHolder(View itemView) {
         super(itemView);
-        mTextView = itemView.findViewById()
+        mTextView = (TextView) itemView.findViewById(R.id.tv_id);
     }
 }
